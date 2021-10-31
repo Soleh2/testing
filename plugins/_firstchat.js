833fb6b39c32400be2db1e8d26b40a508b3629f8
Nando <90469140+Soleh2@users.noreply.github.com>
2021-10-31 22:16:08 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -12,26 +12,26 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
 
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?'}
-`.trim(), '© stikerin', user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
+${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya banting?'}
+`.trim(), '© Moon', user.banned ? 'Pemilik bot' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
 
 module.exports = handler
 function ucapan() {
     const time = moment.tz('Asia/Jakarta').format('HH')
-    res = "Selamat dinihari"
+    res = "Selamat dinihari 🌚"
     if (time >= 4) {
-        res = "Selamat pagi"
+        res = "Selamat pagi 🌄"
     }
     if (time > 10) {
-        res = "Selamat siang"
+        res = "Selamat siang ☀️"
     }
     if (time >= 15) {
-        res = "Selamat sore"
+        res = "Selamat sore 🌇"
     }
     if (time >= 18) {
-        res = "Selamat malam"
+        res = "Selamat malam 🌉"
     }
     return res
 }
