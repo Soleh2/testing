3cbb0491f1af91772d0fbed783696b8df7b3eb5f
Nando <90469140+Soleh2@users.noreply.github.com>
2021-11-03 09:12:51 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,8 +4,9 @@ let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (awa
 ├ Gopay / Dana: 081252848955
 ├ Pulsa: 081336990737
 ├ Qris: Minta owner
-│ _*Donasi ini untuk beli paket data biar bot bisa on*_
+│ 
 └────
+_*Donasi ini untuk membeli kuota, biar botnya bisa on terus*_
 `.trim(), 'Makasih yang udah donasi', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
