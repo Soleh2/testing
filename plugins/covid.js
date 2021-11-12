ae0e2da1fd508f6db1ae3324525181688e672570
Nando <90469140+Soleh2@users.noreply.github.com>
2021-11-12 09:41:41 +0700

    Update covid.js

--- plugins/covid.js
+++ plugins/covid.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
-let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `uhm.. negaranya?\n\ncontoh:\n${usedPrefix + command} indonesia`
-  let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/' + (text)))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
+let handler = async (m, { args, usedPrefix, command }) => {
+    let res = await fetch(global.API('https://covid19.mathdro.id', '/api/countries/'+ (args[0] || '')))
+    if (!res.ok) throw await res.text()
+        let json = await res.json()
+    if (!args[0]) throw `Contoh :\n${usedPrefix}${command} ID\n\n` + json.countries.map((v) => `${v.name} : ${v.iso2} / ${v.iso3}`).join('\n')
   if (json.confirmed) m.reply(`
-Countries : ${text}
+Countries : ${args[0]}
 Confirmed : ${json.confirmed.value}
 Recovered : ${json.recovered.value}
 Deaths : ${json.deaths.value}
