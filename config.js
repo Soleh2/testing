c6fc558427f4ceb4fdc34d170c9a33bece78861e
Nando <90469140+Soleh2@users.noreply.github.com>
2021-11-17 12:14:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.author = 'Moon Bot'
 
 global.wait = 'Tunggu sedang di proses...'
 global.eror = 'Maaf server sedang error'
-global.fla = 'https://telegra.ph/file/8f40e6e8f6ced8baf1cb5.jpg'
+global.fla = 'https://i.ibb.co/z5FZt5j/IMG-20211117-WA0094.jpg'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 
