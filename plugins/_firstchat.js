f6398a34afcc6a75eadc39c827aedd185e1ab276
Soleh2 <90469140+Soleh2@users.noreply.github.com>
2021-11-22 23:44:56 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -1,4 +1,5 @@
 let moment = require('moment-timezone')
+let fetch = require ('node-fetch')
 let handler = m => m
 
 handler.all = async function (m) {
@@ -8,30 +9,31 @@ handler.all = async function (m) {
     if (m.isGroup) return
     if (db.data.settings.groupOnly) return
     let user = global.db.data.users[m.sender]
+    let name = conn.user.name
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
-    await this.sendButton(m.chat, `
+    await conn.send3ButtonLoc(m.chat, await(await fetch(image)).buffer(), `
 Hai, ${ucapan()}
 
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya banting?'}
-`.trim(), '© Moon', user.banned ? 'Pemilik bot' : 'Menu', user.banned ? ',owner' : ',?', m)
+${user.banned ? 'kamu dibanned' : `Saya adalah ${name}, salah satu Bot Whatsapp. harap tidak spam/telpon nomor ini. Ada yang bisa saya bantu?`}
+`.trim(), watermark, user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', 'Panduan penggunaan', '.panduan', 'Rules', '.rules', m, { contextInfo:{externalAdReply: {title: 'Moon', sourceUrl: sumberurl, body: deskripsiurl, thumbnail: thumb}}}, m)
     user.pc = new Date * 1
 }
 
 module.exports = handler
 function ucapan() {
     const time = moment.tz('Asia/Jakarta').format('HH')
-    res = "Selamat dinihari 🌚"
+    res = "Selamat dinihari"
     if (time >= 4) {
-        res = "Selamat pagi 🌄"
+        res = "Selamat pagi"
     }
     if (time > 10) {
-        res = "Selamat siang ☀️"
+        res = "Selamat siang"
     }
     if (time >= 15) {
-        res = "Selamat sore 🌇"
+        res = "Selamat sore"
     }
     if (time >= 18) {
-        res = "Selamat malam 🌉"
+        res = "Selamat malam"
     }
     return res
 }
