fc561efdf11943e3bb1c53282cad85ace8804e1d
Soleh2 <90469140+Soleh2@users.noreply.github.com>
2021-11-22 23:59:36 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -12,7 +12,7 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${ucapan()}
 
-${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya banting?'}
+${user.banned ? 'kamu dibanned' : 'Ada yang bisa saya bantu?\nSaya adalah Moon-Bot, ketik #menu untuk memulai'}
 `.trim(), '© Moon', user.banned ? 'Pemilik bot' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
