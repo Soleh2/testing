1a354efeff429dee118ee88fbb498e7bc34e867f
Soleh2 <90469140+Soleh2@users.noreply.github.com>
2021-11-23 00:14:27 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -434,18 +434,18 @@ function clockString(ms) {
 }
 function ucapan() {
   const time = moment.tz('Asia/Jakarta').format('HH')
-  res = "Selamat dinihari 🌚"
+  res = "Selamat dinihari"
   if (time >= 4) {
-    res = "Selamat pagi 🌄"
+    res = "Selamat pagi"
   }
   if (time > 10) {
-    res = "Selamat siang ☀️"
+    res = "Selamat siang"
   }
   if (time >= 15) {
-    res = "Selamat sore 🌇"
+    res = "Selamat sore"
   }
   if (time >= 18) {
-    res = "Selamat malam 🌉"
+    res = "Selamat malam"
   }
   return res
 }
